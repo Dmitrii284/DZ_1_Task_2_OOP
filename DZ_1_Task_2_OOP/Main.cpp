1c62839e6e5e485824c7aa821baebb19f44251c1
Дмитрий <@ADMIN>
2023-06-17 15:09:09 +0500

    22

--- DZ_1_Task_2_OOP/Main.cpp
+++ DZ_1_Task_2_OOP/Main.cpp
@@ -13,11 +13,17 @@ int main()
 		if (selectFrom == 1) // вывод на экран
 		{
 		A.PrintAllAbonents();
+		A.FileWrit();
+		A.FileRid();
+		continue;
 		}
 		else if (selectFrom == 2)// добавление записи
 		{
 			A.PrintAllAbonents();
 			A.AddAbonent();
+			A.FileWrit();
+			A.FileRid();
+			continue;
 		}
 		else if (selectFrom == 3) // удаление записи
 		{
@@ -25,7 +31,11 @@ int main()
 			int num1;
 				std::cout << "Введите номер для удаления -> ";
 				std::cin >> num1;			
-				A.EraseRec(num1 - 1);
+				A.EraseRec(num1);
+				A.FileWrit();
+				A.FileRid();
+				A.PrintAllAbonents();
+				continue;
 		}
 		else if (selectFrom == 4) //Найти запись.
 		{
@@ -34,8 +44,8 @@ int main()
 			std::cout << "Введите ФИО для поиска абонента\n";
 			std::cin.ignore();
 			std::getline(std::cin, str);
-			A.SubscriberSearch(str);
-			break;
+			A.SubscriberSearch(str);			
+			continue;
 		}
 	} while (selectFrom != 5);
 	return 0;
